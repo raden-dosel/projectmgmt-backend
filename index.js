b84e03fec494a09d3735b7c1b3aba3ac002e01cb
Raden Cedrick Dosel <raden.dosel00@gmail.com>
2023-10-29 23:20:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Connect Database
+connectDB();
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
